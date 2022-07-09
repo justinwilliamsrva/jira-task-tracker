26f1d0c74fa486224832e4b07f5265aa3a2e1541
Justin Williams <justinwdev@gmail.com>
2022-07-09 10:03:53 -0400

    Add basic output functionality.

--- app/Http/Livewire/Output.php
+++ app/Http/Livewire/Output.php
@@ -6,8 +6,27 @@ use Livewire\Component;
 
 class Output extends Component
 {
+    // public $tasks = [];
+
     public function render()
     {
-        return view('livewire.output');
+        return view('livewire.output')->with('tasks', $this->tasks());
     }
+
+    public function tasks()
+    {
+        return session()->has('tasks') ? $this->createNewArray(session('tasks')) : [];
+    }
+
+    public function createNewArray($sessiontasks)
+    {
+        $newArray = [];
+        foreach($sessiontasks as $key => $tasks)
+        {
+            $newArray[$tasks['task']][] = ['time'=>$key,'work'=> $tasks['work']];
+        }
+
+        return $newArray;
+    }
+
 }
--- resources/views/livewire/output.blade.php
+++ resources/views/livewire/output.blade.php
@@ -1,3 +1,19 @@
-<div>
-Output
+<div class="flex flex-col space-y-6 py-2">
+    <h1 class="text-center text-2xl">Output</h1>
+    <div class="flex flex-col space-y-2">
+        @foreach($tasks as $key => $task)
+            <div class="border-2">
+                <h2>{{$key}}</h2>
+                @foreach($task as $t)
+                    <div class="flex justify-around">
+                        <p>{{$t['time']}}</p>
+                        <p>{{$t['work']}}</p>
+                    </div>
+                @endforeach
+            </div>
+        @endforeach
+    </div>
+
+
+
 </div>
