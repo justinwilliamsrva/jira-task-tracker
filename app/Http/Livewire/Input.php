a799247ed9b5be2fc9344e43ec30041ad7ab0213
Justin Williams <justinwdev@gmail.com>
2022-07-09 08:06:29 -0400

    Add sessions data get and put to input.

--- app/Http/Livewire/Input.php
+++ app/Http/Livewire/Input.php
@@ -9,8 +9,20 @@ class Input extends Component
     public array $timeArray = [
         '8:30','9:00','9:30','10:00','10:30','11:00','11:30','12:00','12:30','13:00','13:30','14:00','14:30','15:00','15:30','16:00','16:30','17:00','17:30',
     ];
+    public $task = [];
     public function render()
     {
         return view('livewire.input');
+    }
+
+    public function mount()
+    {
+        $this->task = session('tasks');
+    }
+
+    public function save()
+    {
+        session(['tasks' => $this->task]);
+
     }
 }
--- resources/views/livewire/input.blade.php
+++ resources/views/livewire/input.blade.php
@@ -1,17 +1,21 @@
 <div>
-    <form class="flex flex-col space-y-6 py-2" wire:submit.prevent="search">
+    <form class="flex flex-col space-y-6 py-2" wire:submit.prevent="save">
         <h1 class="text-center text-2xl">Input</h1>
         <div class="flex justify-center space-x-3 mx-auto">
             <button wire:click="clear" class="py-2 px-8 bg-blue-200">Clear</button>
             <button type="submit" class="py-2 px-8 bg-green-200">Save</button>
         </div>
         <div class="space-y-2">
+
             @foreach($timeArray as $time)
                 <div class="flex justify-around bg-blue-500 p-2">
                     <h2>{{ $time }}</h2>
-                    <input type="hidden" wire:model="time[]" value="{{ $time }}">
-                    {{--<input type="text" wire:model="task[]" placeholder="Task">
-                    <textarea cols="30" wire:model="completed[]" placeholder="Completed Work"></textarea>--}}
+                    <input type="text" wire:model="task.{{$time}}.task" value="{{ $sessionTasks[$time]['task'] ?? '' }}" autocomplete>
+                    <textarea cols="30" wire:model="task.{{$time}}.work" value="{{ $sessionTasks[$time]['work'] ?? '' }}"></textarea>
+                    <label>
+                        Entered in Jira
+                        <input type="checkbox" wire:model="task.{{$time}}.completed">
+                    </label>
                 </div>
             @endforeach
         </div>
