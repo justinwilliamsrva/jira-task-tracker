793bcca8807ae0a675ba40074d93b6cf9441670a
Justin Williams <justin.williams@guidanceresidential.com>
2023-01-25 22:06:18 -0500

    Fix nav link to page if scrolled on medium screen or smaller.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,35 +14,38 @@
 
     @vite('resources/js/app.js')
     @livewireStyles
-
-
     <style>
         body {
             font-family: 'Nunito', sans-serif;
         }
     </style>
 </head>
-
 <body class="antialiased">
-
-
     <div>
         <div class="container max-w-5xl mx-auto mt-10 space-y-4">
-            <h1 class=" text-center text-5xl">Jira Task Tracker</h1>
-            <div class="ml-2">
-                <a class="{{ Route::currentRouteName() ==  'input' ? 'hidden' : 'py-2 px-8 bg-blue-200 hover:bg-blue-500'}}" href="{{route('input')}}"><button >Add Time</button> </a>
-                <a class="{{ Route::currentRouteName() ==  'output' ? 'hidden' : 'py-2 px-8 bg-green-200 hover:bg-green-500'}}"href="{{route('output')}}"><button>View Time</button> </a>
-
+            <h1 class=" text-center text-5xl px-4">Jira Task Tracker</h1>
+            <div id="change-page" class="ml-2 z-50 lg:relative">
+                <a class="{{ Route::currentRouteName() ==  'input' ? 'hidden' : 'py-2 px-8 bg-blue-200 hover:bg-blue-500'}}" href="{{route('input')}}"><button >Add Time</button></a>
+                <a class="{{ Route::currentRouteName() ==  'output' ? 'hidden' : 'py-2 px-8 bg-green-200 hover:bg-green-500'}}"href="{{route('output')}}"><button>View Time</button></a>
             </div>
-
-
             <div class="">
                {{ $slot }}
             </div>
         </div>
     </div>
-
     @livewireScripts
 </body>
+<script>
+window.onscroll = function() {
+    var changePage = document.getElementById("change-page");
+    if (window.pageYOffset > 80 && screen.width >= 380) {
+        changePage.classList.add("fixed", "top-0.5");
+    } else if (window.pageYOffset > 135 && screen.width < 380) {
+    changePage.classList.add("fixed", "top-0.5");
+    } else {
+        changePage.classList.remove("fixed", "top-0.5");
+    }
+}
 
+</script>
 </html>
